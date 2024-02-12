7eb53147aed5a6325d71852fe9f55a7f3820564d
fznoif <fbzjifz@gmail.com>
2024-02-12 21:58:54 +0100

    setup some things

--- sources/main.py
+++ sources/main.py
@@ -1,2 +1,10 @@
-print('fichier main')
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
