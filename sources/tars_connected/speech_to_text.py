4e1a8d993758241b8939a98590a702da9ab4bf3f
Olivier SERRA <olivier.skyworld@gmail.com>
2024-02-21 00:05:22 +0100

    MyUpload

--- sources/tars_connected/speech_to_text.py
+++ sources/tars_connected/speech_to_text.py
@@ -8,6 +8,19 @@ import speech_recognition
 
 # import text_to_response
 
+def turn_in_red():
+    return
+    # turn_in_red() est une foncion provisoire, qui devra être remplacée par un ACTIVATEUR de témoin d'écoute
+def turn_in_black():
+    return
+    # turn_in_black() est une foncion provisoire, qui devra être remplacée par un DESACTIVATEUR de témoin d'écoute
+def say():
+    return
+    # say(text) est une fonction provisoire, qui devra être remplacée par un text to speech
+def reponse_a_la_question():
+    return
+    # reponse_a_la_question(text) est une fonction provisoire, qui devra être remplacée par un text to response
+
 class Tars:
     def __init__(self):
         # pass
@@ -22,7 +35,6 @@ class Tars:
             print("Parlez...")
             while True:
                 try:
-                    self.recognizer.adjust_for_ambient_noise(mic, duration=1)       # Se calibre pendant 2 sec pour en ignorer le son ambiant
                     audio = self.recognizer.listen(mic)                 # Ecoute jusqu'à ce qu'il détecte un vide
                     text = self.recognizer.recognize_google(audio, language="fr-FR")
                     text = text.lower()
@@ -30,34 +42,45 @@ class Tars:
                 
                     if text in self.mots_de_fin:
                         print("Au revoir !")
-                        break
+                        return
+                    
+                    if "tarse " in text:
+                        turn_in_red()
+                        text = text.split("tarse ")[1]
+                        say(reponse_a_la_question(text))
+                        turn_in_black()
+                    
+                    elif "torse " in text:
+                        turn_in_red()
+                        text = text.split("torse ")[1]
+                        say(reponse_a_la_question(text))
+                        turn_in_black()
 
-                    if "tarse" in text:
+                    elif "tarse" in text or "torse" in text:
                         print("J'écoute ?...")
+                        turn_in_red()
                         while True:
                             try:
-                                self.recognizer.adjust_for_ambient_noise(mic, duration=1)       # Se calibre pendant 2 sec pour en ignorer le son ambiant
-                                audio = self.recognizer.listen(mic)                 # Ecoute jusqu'à ce qu'il détecte un vide
+                                audio = self.recognizer.listen(mic)           # Ecoute jusqu'à ce qu'il détecte un vide
                                 text = self.recognizer.recognize_google(audio, language="fr-FR")
                                 text = text.lower()
 
                                 if text in self.mots_de_fin:
                                     print("Au revoir !")
-                                    # self.assistant.stop()
-                                    # sys.exit()
-                                    break
+                                    turn_in_black()
+                                    return
                                 else:
-                                    response = self.assistant.request(text)
-                                    if response is not None:
-                                        print(response)
+                                    print("Le Lorem Ipsum est simplement du faux texte employé en imprimerie.")
+                                    say(reponse_a_la_question(text))
+                                    turn_in_black()
+                                    break
 
                             except speech_recognition.RequestError:
-                                print(">>>> Error: API was unreachable or unresponsive")                      # API was unreachable or unresponsive
-                                print(">>>> Veuillez répéter")
+                                print(">>>> Error: API was unreachable or unresponsive")          # API was unreachable or unresponsive
+                                print(">>>> Problème de connexion avec l'API")
 
                             except speech_recognition.UnknownValueError:
-                                print(">>>> Error: Unable to recognize speech, speech was unintelligible")              # speech was unintelligible
-                                print(">>>> Veuillez répéter")
+                                print(">>>> Pardon, veuillez répéter")
 
                 except speech_recognition.RequestError:
                     continue
