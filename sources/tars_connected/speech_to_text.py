5407f9682188e055c9e118fc48c7529e0fa54dc2
Olivier SERRA <olivier.skyworld@gmail.com>
2024-02-20 20:11:38 +0100

    Upload

--- sources/tars_connected/speech_to_text.py
+++ sources/tars_connected/speech_to_text.py
@@ -1,11 +1,12 @@
 import sys
 
 
-# from openai import OpenAI
 import speech_recognition
+# from openai import OpenAI
 # import pyttsx3 as tts
-
 # from neuralintents.assistants import BasicAssistant
+
+# import text_to_response
 
 class Tars:
     def __init__(self):
@@ -14,54 +15,58 @@ class Tars:
         # self.assistant.fit_model(epochs=50)
         # self.assistant.save_model()
 
+        self.mots_de_fin = ["couper", "arrêter", "stoppe", "stopper", "au revoir", "arrête"]
         self.recognizer = speech_recognition.Recognizer()           # Initialiser Speech Recognition
 
-        while True:
-            with speech_recognition.Microphone() as mic:            # Avec le microphone par défaut
+        with speech_recognition.Microphone() as mic:            # Avec le microphone par défaut
+            print("Parlez...")
+            while True:
                 try:
-                    self.recognizer.adjust_for_ambient_noise(mic, duration=2)       # Se calibre pendant 2 sec pour en ignorer le son ambiant
-                    print("Parlez...")
+                    self.recognizer.adjust_for_ambient_noise(mic, duration=1)       # Se calibre pendant 2 sec pour en ignorer le son ambiant
                     audio = self.recognizer.listen(mic)                 # Ecoute jusqu'à ce qu'il détecte un vide
                     text = self.recognizer.recognize_google(audio, language="fr-FR")
                     text = text.lower()
-                    print(f"J'ai entendu: \'\'\'\'{text}\'\'\'\'")
+                    print(f"<{text}>")
                 
-                    if text == "couper":
+                    if text in self.mots_de_fin:
                         print("Au revoir !")
                         break
 
                     if "tarse" in text:
                         print("J'écoute ?...")
-                        try:
-                            self.recognizer.adjust_for_ambient_noise(mic, duration=2)       # Se calibre pendant 2 sec pour en ignorer le son ambiant
-                            audio = self.recognizer.listen(mic)                 # Ecoute jusqu'à ce qu'il détecte un vide
-                            text = self.recognizer.recognize_google(audio, language="fr-FR")
-                            text = text.lower()
+                        while True:
+                            try:
+                                self.recognizer.adjust_for_ambient_noise(mic, duration=1)       # Se calibre pendant 2 sec pour en ignorer le son ambiant
+                                audio = self.recognizer.listen(mic)                 # Ecoute jusqu'à ce qu'il détecte un vide
+                                text = self.recognizer.recognize_google(audio, language="fr-FR")
+                                text = text.lower()
 
-                            if text == "couper":
-                                print("Au revoir !")
-                                # self.assistant.stop()
-                                # sys.exit()
-                                break
-                            # else:
-                            #     if text is not None:
-                                    # response = self.assistant.request(text)
-                                    # if response is not None:
-                                    #     print(response)
+                                if text in self.mots_de_fin:
+                                    print("Au revoir !")
+                                    # self.assistant.stop()
+                                    # sys.exit()
+                                    break
+                                else:
+                                    response = self.assistant.request(text)
+                                    if response is not None:
+                                        print(response)
 
-                        except speech_recognition.RequestError:
-                            print("Error: API was unreachable or unresponsive")                      # API was unreachable or unresponsive
+                            except speech_recognition.RequestError:
+                                print(">>>> Error: API was unreachable or unresponsive")                      # API was unreachable or unresponsive
+                                print(">>>> Veuillez répéter")
 
-                        except speech_recognition.UnknownValueError:
-                            print("Error: Unable to recognize speech, speech was unintelligible")              # speech was unintelligible
+                            except speech_recognition.UnknownValueError:
+                                print(">>>> Error: Unable to recognize speech, speech was unintelligible")              # speech was unintelligible
+                                print(">>>> Veuillez répéter")
 
                 except speech_recognition.RequestError:
-                    print("Error: API was unreachable or unresponsive")                      # API was unreachable or unresponsive
+                    continue
 
                 except speech_recognition.UnknownValueError:
-                    print("Error: Unable to recognize speech, speech was unintelligible")              # speech was unintelligible
-                
-                text = None
+                    continue
+
+
+
 
 
 Bot = Tars()
