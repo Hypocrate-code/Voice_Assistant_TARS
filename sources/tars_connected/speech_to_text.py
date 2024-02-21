137712221549a7da1cae9b762298355185ae6c4c
Olivier SERRA <olivier.skyworld@gmail.com>
2024-02-21 10:08:01 +0100

    OKK

--- sources/tars_connected/speech_to_text.py
+++ sources/tars_connected/speech_to_text.py
@@ -8,14 +8,14 @@ import speech_recognition
 
 # import text_to_response
 
-def turn_in_red():
-    print("> On a appelé turn_in_red")
-    return
-    # turn_in_red() est une foncion provisoire, qui devra être remplacée par un ACTIVATEUR de témoin d'écoute
-def turn_in_black():
-    print("> On a appelé turn_in_black")
-    return
-    # turn_in_black() est une foncion provisoire, qui devra être remplacée par un DESACTIVATEUR de témoin d'écoute
+# def turn_in_red():
+#     print("> On a appelé turn_in_red")
+#     return
+#     turn_in_red() est une foncion provisoire, qui devra être remplacée par un ACTIVATEUR de témoin d'écoute
+# def turn_in_black():
+#     print("> On a appelé turn_in_black")
+#     return
+#     turn_in_black() est une foncion provisoire, qui devra être remplacée par un DESACTIVATEUR de témoin d'écoute
 def say(text):
     print("> On a appelé say")
     return
@@ -31,39 +31,39 @@ class Tars:
         # self.assistant.fit_model(epochs=50)
         # self.assistant.save_model()
 
+        self.mots_activation = ["tars", "tarse", "torse", "tarz"]
         self.mots_de_fin = ["couper", "arrêter", "stoppe", "stopper", "au revoir", "arrête"]
         self.recognizer = speech_recognition.Recognizer()           # Initialiser Speech Recognition
 
         with speech_recognition.Microphone() as mic:            # Avec le microphone par défaut
-            print("Parlez...")
+            self.recognizer.adjust_for_ambient_noise(mic, duration=4)
             while True:
+                print("Parlez...")
                 try:
                     audio = self.recognizer.listen(mic)                 # Ecoute jusqu'à ce qu'il détecte un vide
                     text = self.recognizer.recognize_google(audio, language="fr-FR")
                     text = text.lower()
                     print(f"<{text}>")
-                
-                    if text in self.mots_de_fin:
+
+
+
+
+
+                    for i in range(len(self.mots_activation)):
+                        if self.mots_activation[i]+" " in text:
+                            text = text.split(f"{self.mots_activation[i]} ")[1]
+                            print(f"\nVotre question: {text}\n")
+                            say(reponse_a_la_question(text))
+                            break
+
+
+
+                    if text in self.mots_de_fin or "fin de requête" in text:
                         print("Au revoir !")
                         return
-                    
-                    if "tarse " in text:
-                        turn_in_red()
-                        text = text.split("tarse ")[1]
-                        print(f"\nVotre question: {text}\n")
-                        say(reponse_a_la_question(text))
-                        turn_in_black()
-                    
-                    elif "torse " in text:
-                        turn_in_red()
-                        text = text.split("torse ")[1]
-                        print(f"\nVotre question: {text}\n")
-                        say(reponse_a_la_question(text))
-                        turn_in_black()
 
-                    elif "tarse" in text or "torse" in text:
+                    elif self.mots_activation[0] in text or self.mots_activation[1] in text or self.mots_activation[2] in text or self.mots_activation[3] in text:
                         print("J'écoute ?...")
-                        turn_in_red()
                         while True:
                             try:
                                 audio = self.recognizer.listen(mic)           # Ecoute jusqu'à ce qu'il détecte un vide
@@ -72,12 +72,10 @@ class Tars:
 
                                 if text in self.mots_de_fin:
                                     print("Au revoir !")
-                                    turn_in_black()
                                     return
                                 else:
                                     print(f"\nVotre question: {text}\n")
                                     say(reponse_a_la_question(text))
-                                    turn_in_black()
                                     break
 
                             except speech_recognition.RequestError:
@@ -91,6 +89,7 @@ class Tars:
                     continue
 
                 except speech_recognition.UnknownValueError:
+                    print(">>>> Pardon, veuillez répéter")
                     continue
 
 
